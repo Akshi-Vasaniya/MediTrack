88d8e4f73222c901f5a5d9f6ccb166099d682a52
dhavanikgithub <kanzariyagovindbhai21@gnu.ac.in>
2023-11-11 11:12:34 +0530

    spell check, clear form after medicine added, bugs fix, optimize code, rearrange the code

--- app/src/main/java/com/example/meditrack/dataModel/api/ApiInterface.kt
+++ app/src/main/java/com/example/meditrack/dataModel/api/ApiInterface.kt
@@ -2,6 +2,8 @@ package com.example.meditrack.dataModel.api
 
 import com.example.meditrack.dataModel.dataClasses.SearchItemData
 import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -14,5 +16,7 @@ interface ApiInterface {
     @GET("/medicine_web_scrap/{medicinename}")
     fun insertDocument(@Path("medicinename") medicinename: String?): Call<JsonElement>
 
+    @GET("/spell_correct/{medicinename}")
+    fun spellCorrect(@Path("medicinename") medicinename: String?): Call<JsonElement>
 
 }
